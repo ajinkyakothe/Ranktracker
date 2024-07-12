add50bc8704e619a766c3daa0c11e131ff3bd36b
Ajinkya Kothe <115250148+ajinkyakothe@users.noreply.github.com>
2024-07-12 21:24:54 +0530

    Update rankingFunctions.js

--- src/libs/rankingFunctions.js
+++ src/libs/rankingFunctions.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 export async function doGoogleSearch(keyword) {
   const data = { country: "us", query: { q: keyword }, brd_json: "json" };
   const url =
-    "https://api.brightdata.com/serp/req?customer=hl_e7fe7ce4&zone=rank";
+    "bright data api link ";
   const headers = {
-    Authorization: "Bearer 8775d128-22f7-4dc5-9105-e801b5e35fe5",
+    Authorization: "token here",
   };
   const response = await axios.post(url, data, { headers });
   // return response.headers.get("x-response-id");
